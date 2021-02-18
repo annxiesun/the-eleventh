084bc166724ef5640728453a21eea4429e41986e
Annie <annievampirerox@gmail.com>
2021-02-18 06:48:46 -0500

    added more to titlepage

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -5,25 +5,53 @@ import FadeIn from 'react-fade-in';
 
 class Homepage extends React.Component {
   constructor() {
-	super();
-   }
-   
+    super();
+  }
+
 
   render() {
+    //<img className="image-logo-main" src={"/assets/text_logo.png"}/>
     return (
-        <FadeIn>
-        <div>
-             
+      <FadeIn>
+        <div class="layout">
+          <div>
+            <div className="collage pic1">
+              <img className="girl" src={"/assets/calendar-mockup1.png"} />
+            </div>
 
-        <div className="home-block">
-            <img className="image-logo-main" src={"/assets/text_logo.png"}/>
-            <button onClick={this.props.onClick} className="fill-button create-btn">
+            <div className="collage pic2">
+              <img className="city" src={"/assets/calendar-mockup2.png"} />
+            </div>
+            <div>
+              <div class="ro-title">
+              <div class="skew-title">
+                <div id="the">the</div>
+                <div id="eleventh">eleventh</div>
+              </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="home-block">
+            <div class="subt-text">NOVEL & UNIQUE CALENDARS</div>
+            <div class="desc-text">Turn your calendars into wall-art that speaks to you. New customizable hand-drawn designs for every month.
+Each sheet is printed on 12 x 13 65lb canvas paper, with the highest quality of ink.</div>
+            <button id="subscribe" className="fill-button create-btn">
+              SUBSCRIBE
+                </button>
+            <div class="btn-row">
+              <button onClick={this.props.onClick} className="fill-button create-btn">
                 CREATE CALENDAR
                 </button>
+
+              <button onClick={this.props.onClick} className="fill-button create-btn">
+                FIND BIRTH DAY
+                </button>
             </div>
-            </div>
-            </FadeIn>
-    
+          </div>
+        </div>
+      </FadeIn>
+
     );
   }
 }
